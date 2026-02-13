7116b37d3356fc151c6452ff3a54490d672f7ba9
zimri <zimrilopez2006@gmail.com>
2026-02-13 01:40:54 -0600

    Se actualizo transaction routes tarea 20

--- src/transactions/transaction.routes.js
+++ src/transactions/transaction.routes.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { createTransaction } from './transaction.controller.js';
-import { validateTransaction } from '../middlewares/transaction.middleware.js';
+import { validateTransaction } from '../../middlewares/transaction.middleware.js';
 
 const router = Router();
 
 router.post(
-    '/', 
+    '/create',
     validateTransaction,
-     createTransaction);
+    createTransaction
+);
 
 export default router;
+
